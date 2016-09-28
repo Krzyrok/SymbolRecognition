6fab544b4367d48ae547aa4670c881a9e3140b43
Krzyrok <krzyrok@gmail.com>
2016-09-28 15:34:20 +0200

    clean up ShellViewModel (removed content)

--- SymbolRecognition/SymbolRecognition/ViewModels/ShellViewModel.cs
+++ SymbolRecognition/SymbolRecognition/ViewModels/ShellViewModel.cs
@@ -4,23 +4,5 @@ namespace SymbolRecognition.ViewModels
 {
     public class ShellViewModel : PropertyChangedBase
     {
-        private readonly IUserService _userService;
-
-        public ShellViewModel(IUserService userService)
-        {
-            _userService = userService;
-            MyName = "Insert your name";
-        }
-
-        public string MyName { get; set; }
-
-        public string FullName => $"{MyName} {_userService.GetSampleUserLastName()}";
-
-        public void ResetName()
-        {
-            MyName = "name after resetting";
-        }
-
-        public bool CanResetName => FullName.Length > 10;
     }
 }
