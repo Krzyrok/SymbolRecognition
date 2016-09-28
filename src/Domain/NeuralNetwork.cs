a0ed395d9aa7a7b550a09e989a53369b4cf00a4e
Krzyrok <krzyrok@gmail.com>
2016-09-28 18:06:16 +0200

    added integration test for learning and recognising symbol

--- src/Domain/NeuralNetwork.cs
+++ src/Domain/NeuralNetwork.cs
@@ -1,6 +1,16 @@
-﻿namespace Domain
+﻿using System.Collections.Generic;
+
+namespace Domain
 {
     public class NeuralNetwork
     {
+        public void Learn(IEnumerable<Symbol> symbolsToLearn)
+        {
+        }
+
+        public bool TryRecognise(Symbol symbolToRecognise)
+        {
+            return true;
+        }
     }
 }
---
+++ src/Tests/Integration/NeuralNetworkTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Domain;
+using Xunit;
+
+namespace Tests.Integration
+{
+    public class NeuralNetworkTests
+    {
+        [Fact]
+        public void ShouldRecogniseLearnedSymbol()
+        {
+            // given
+            var neuralNetwork = new NeuralNetwork();
+            var symbolUsedForLearning = new Symbol();
+            var symbolsToLearn = new List<Symbol> { symbolUsedForLearning, new Symbol()};
+            neuralNetwork.Learn(symbolsToLearn);
+
+            // when
+            bool symbolIsRecognised = neuralNetwork.TryRecognise(symbolUsedForLearning);
+
+            // then
+            Assert.True(symbolIsRecognised);
+        }
+    }
+}
