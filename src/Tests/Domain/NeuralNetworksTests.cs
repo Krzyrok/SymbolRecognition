a65cea74d678c158088dfe6ef7ae480fc8d7c6de
Krzyrok <krzyrok@gmail.com>
2016-09-28 17:40:07 +0200

    added xunit and test project with simple test for creating NN

---
+++ src/Tests/Domain/NeuralNetworksTests.cs
@@ -0,0 +1,18 @@
+﻿using Domain;
+using Xunit;
+
+namespace Tests.Domain
+{
+    public class NeuralNetworksTests
+    {
+        [Fact]
+        public void ShouldCreateNeuralNetwork()
+        {
+            // when
+            var neuralNetwork = new NeuralNetwork();
+
+            // then
+            Assert.NotNull(neuralNetwork);
+        }
+    }
+}
