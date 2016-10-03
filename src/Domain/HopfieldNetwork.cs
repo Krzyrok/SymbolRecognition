b82e02ae7bc98e188f2168f6e5043a6ef4088dcf
Krzyrok <krzyrok@gmail.com>
2016-10-03 18:43:18 +0200

    added Neuron and symbol recognition in HN
    small refactor of LearnWithHebb method in HN

--- src/Domain/HopfieldNetwork.cs
+++ src/Domain/HopfieldNetwork.cs
@@ -9,10 +9,13 @@ namespace Domain
         private int NumberOfNeurons => _symbolsIn.GetLength(1); // N; TODO: remove comment
         private int NumberOfStoredSymbols => _symbolsIn.GetLength(0); // M; TODO: remove comment
 
-        private int[,] _symbolsIn;
+        private int[,] _symbolsIn; // Bipolar
+        private int[,] _symbolsOut; // Binary
+        private const int MaxIterations = 20; // 
 
         // TODO: remove getter and refactor UT
-        public int[,] Weights { get; private set; }
+        // TODO: maybe Weights could be separate class - class for storing collection
+        public int[,] Weights { get; private set; } // W
 
         public void Learn(ICollection<BipolarSymbol> symbolsToLearn)
         {
@@ -21,12 +24,11 @@ namespace Domain
                 throw new NoSymbollsPassedException();
             }
 
-            StoreSymbolsValues(symbolsToLearn);
+            StoreSymbolsValuesForLearning(symbolsToLearn);
             LearnWithHebb();
-
         }
 
-        private void StoreSymbolsValues(ICollection<BipolarSymbol> symbolsToLearn)
+        private void StoreSymbolsValuesForLearning(ICollection<BipolarSymbol> symbolsToLearn)
         {
             _symbolsIn = new int[symbolsToLearn.Count, BipolarSymbol.RowSize * BipolarSymbol.ColumnSize];
 
@@ -35,40 +37,123 @@ namespace Domain
             {
                 var valueIndex = 0;
                 for (var rowIndex = 0; rowIndex < BipolarSymbol.RowSize; rowIndex++)
+                {
                     for (var columnIndex = 0; columnIndex < BipolarSymbol.ColumnSize; columnIndex++)
                     {
                         _symbolsIn[symbolIndex, valueIndex++] = bipolarSymbol.Values[rowIndex, columnIndex];
                     }
+                }
 
                 symbolIndex++;
             }
         }
 
-        // TODO: draw diagram and refactor this
+        // TODO: draw diagram (which neuron with which, what indexes for weights corresponds to them) and refactor this
         private void LearnWithHebb()
         {
             Weights = new int[NumberOfNeurons, NumberOfNeurons];
 
-            for (var i = 0; i < NumberOfNeurons; i++)
+            for (var weightFirstIndex = 0; weightFirstIndex < NumberOfNeurons; weightFirstIndex++)
             {
-                for (var j = 0; j <= i; j++)
+                for (var weightSecondIndex = 0; weightSecondIndex <= weightFirstIndex; weightSecondIndex++)
                 {
-                    Weights[i, j] = 0;
-                    if (j < i)
+                    Weights[weightFirstIndex, weightSecondIndex] = 0;
+                    if (weightSecondIndex < weightFirstIndex)
                     {
-                        for (var m = 0; m < NumberOfStoredSymbols; m++)
+                        for (var storedSymbolIndex = 0; storedSymbolIndex < NumberOfStoredSymbols; storedSymbolIndex++)
                         {
-                            Weights[i, j] += _symbolsIn[m, i] * _symbolsIn[m, j];
+                            Weights[weightFirstIndex, weightSecondIndex] += _symbolsIn[storedSymbolIndex, weightFirstIndex] * _symbolsIn[storedSymbolIndex, weightSecondIndex];
                         }
                     }
-                    Weights[j, i] = Weights[i, j];
+                    Weights[weightSecondIndex, weightFirstIndex] = Weights[weightFirstIndex, weightSecondIndex];
                 }
             }
         }
 
         public bool TryRecognise(BipolarSymbol symbolToRecognise)
         {
+            StoreSymbolValuesForRecognising(symbolToRecognise);
+            Recognise();
+
             return true;
+        }
+
+        private void StoreSymbolValuesForRecognising(BipolarSymbol symbolToRecognise)
+        {
+            _symbolsIn = new int[1, BipolarSymbol.RowSize * BipolarSymbol.ColumnSize];
+            _symbolsOut = new int[MaxIterations, BipolarSymbol.RowSize * BipolarSymbol.ColumnSize];
+
+            var digitValueIndex = 0;
+            for (var rowIndex = 0; rowIndex < BipolarSymbol.RowSize; rowIndex++)
+            {
+                for (var columnIndex = 0; columnIndex < BipolarSymbol.ColumnSize; columnIndex++)
+                {
+                    _symbolsIn[0, digitValueIndex++] = symbolToRecognise.Values[rowIndex, columnIndex];
+                }
+            }
+        }
+
+        private void Recognise()
+        {
+            var iterationsCountOfRecognising = 0;
+            var delta = 0;
+
+            // M == NumberOfStoredSymbols, N == NumberOfNeurons; M == 1 bo rozpoznajemy tylko jeden symbol
+            var neuronsInputs = new int[1, NumberOfNeurons];  // X
+            for (var neuronIndex = 0; neuronIndex < NumberOfNeurons; neuronIndex++)
+                neuronsInputs[0, neuronIndex] = _symbolsIn[0, neuronIndex]; // patterns
+
+            var neurons = new Neuron[NumberOfNeurons]; // cells
+            for (var neuronIndex = 0; neuronIndex < NumberOfNeurons; neuronIndex++)
+                neurons[neuronIndex] = new Neuron();
+
+            while (true)
+            {
+                var neuronsInputsWereChanged = false;
+
+                for (var neuronNumber = 0; neuronNumber < NumberOfNeurons; neuronNumber++)
+                {
+                    neurons[neuronNumber].WeightsSum(neuronsInputs, Weights, 0, neuronNumber);  // threshold can be = X[m, i] = symbolValues[0, neuronNumber]
+                    neurons[neuronNumber].ActivationFunction(neurons[neuronNumber].WeightsSumOutput, neuronsInputs[0, neuronNumber]);
+
+                    if (neuronsInputs[0, neuronNumber] != neurons[neuronNumber].ActivationFunctionOutput)
+                    {
+                        neuronsInputs[0, neuronNumber] = neurons[neuronNumber].ActivationFunctionOutput;
+                        neuronsInputsWereChanged = true;
+                    } 
+                }
+
+                if (neuronsInputsWereChanged)
+                {
+                    iterationsCountOfRecognising++; // TODO: remove because for debugging purposes
+                }
+
+                if (!neuronsInputsWereChanged)
+                {
+                    break;
+                }
+            }
+
+            BipolarToBinary(neuronsInputs);
+
+            for (var neuronIndex = 0; neuronIndex < NumberOfNeurons; neuronIndex++)
+                _symbolsOut[0, neuronIndex] = neuronsInputs[0, neuronIndex];
+        }
+
+        private static void BipolarToBinary(int[,] symbols)
+        {
+            int rowsCount = symbols.GetLength(0);
+            int columnsCount = symbols.GetLength(1);
+            for (var row = 0; row < rowsCount; row++)
+                for (var column = 0; column < columnsCount; column++)
+                {
+                    symbols[row, column] = BipolarToBinary(symbols[row, column]);
+                }
+        }
+
+        private static int BipolarToBinary(int bipolarValue)
+        {
+            return bipolarValue == 1 ? 1 : 0;
         }
     }
 }
---
+++ src/Domain/Neuron.cs
@@ -0,0 +1,40 @@
+﻿namespace Domain
+{
+    public class Neuron
+    {
+        public int WeightsSumOutput; // U
+        public int ActivationFunctionOutput; // Y
+
+        public void ActivationFunction(int aggregatedFeedbackSignals, int inputSignalBeforeResponse)
+        {
+            if (aggregatedFeedbackSignals > 0)
+            {
+                ActivationFunctionOutput = 1;
+            }
+            else if (aggregatedFeedbackSignals == 0)
+            {
+                if (inputSignalBeforeResponse > 0)
+                    ActivationFunctionOutput = 1;
+                else
+                    ActivationFunctionOutput = -1;
+            }
+            else if (aggregatedFeedbackSignals < 0)
+            {
+                ActivationFunctionOutput = -1;
+            }
+        }
+
+        // int[,]X - bipolar or binary patterns array
+        // int[,] weights - before response
+        // TODO: X as one dimensional array
+        // TODO: remove threshold because unused
+        public void WeightsSum(int[,] X, int[,] weights, int patternNumber, int neuronNumber)
+        {
+            WeightsSumOutput = X[patternNumber, neuronNumber];
+
+            int weightsDimension = weights.GetLength(0);
+            for (var weightIndex = 0; weightIndex < weightsDimension; weightIndex++)
+                WeightsSumOutput += weights[neuronNumber, weightIndex] * X[patternNumber, weightIndex];
+        }
+    }
+}
