555e721664c604db28bd1a8a41e6753e626afebb
Krzyrok <krzyrok@gmail.com>
2016-10-05 19:09:14 +0200

    added UT for converting to on dimensional array

--- src/Tests/Domain/SymbolValuesTest.cs
+++ src/Tests/Domain/SymbolValuesTest.cs
@@ -35,5 +35,32 @@ namespace Tests.Domain
             var exception = Assert.Throws<ArgumentException>(creatingSymbol);
             Assert.True(exception.Message.Contains("column"));
         }
+
+        [Fact]
+        public void ShouldConvertToOneDimensionalArray()
+        {
+            // given
+            var symbolValues = new int[SymbolValues.RowSize, SymbolValues.ColumnSize];
+            symbolValues[0, 1] = 2;
+            symbolValues[2, 3] = 3;
+            var symbol = new SymbolValues(symbolValues, DoNotConvert);
+
+            // when
+            int[] symbolValuesArray = symbol.ConvertToOneDimensionalArray();
+
+            // then
+            var expectedArray = new[]
+            {
+                0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+            };
+            Assert.Equal(expectedArray, symbolValuesArray);
+        }
     }
 }
